c6b995c3d7154cf51e5968c22fb6b08b16de145a
tnqls0624 <56984613+tnqls0624@users.noreply.github.com>
2021-03-17 11:40:34 +0900

    Updates

---
+++ precache-manifest.f7da061938a7a4dc73c633bc92108801.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "146451f0555f657a6378",
+    "url": "/sportsstore/static/css/main.2a0ebbe5.chunk.css"
+  },
+  {
+    "revision": "146451f0555f657a6378",
+    "url": "/sportsstore/static/js/main.146451f0.chunk.js"
+  },
+  {
+    "revision": "0ebc6a6877266a2c92c6",
+    "url": "/sportsstore/static/css/1.5588ea2c.chunk.css"
+  },
+  {
+    "revision": "0ebc6a6877266a2c92c6",
+    "url": "/sportsstore/static/js/1.0ebc6a68.chunk.js"
+  },
+  {
+    "revision": "b86d15f353f10db34d08",
+    "url": "/sportsstore/static/js/runtime~main.b86d15f3.js"
+  },
+  {
+    "revision": "f80b8a05dd76fdb40e630f1e90b1303d",
+    "url": "/sportsstore/static/media/fa-brands-400.f80b8a05.woff2"
+  },
+  {
+    "revision": "5e405c16c959bec402cc00609e9806f9",
+    "url": "/sportsstore/static/media/fa-brands-400.5e405c16.woff"
+  },
+  {
+    "revision": "877baf6278a6f1506a07afd23b334f47",
+    "url": "/sportsstore/static/media/fa-brands-400.877baf62.eot"
+  },
+  {
+    "revision": "ec813c5b36705e64ba121073b315cb52",
+    "url": "/sportsstore/static/media/fa-regular-400.ec813c5b.eot"
+  },
+  {
+    "revision": "418d54a4a9df0e7b3e28dc09e4cce808",
+    "url": "/sportsstore/static/media/fa-brands-400.418d54a4.ttf"
+  },
+  {
+    "revision": "06b3153d3d537a854dc839652018bb87",
+    "url": "/sportsstore/static/media/fa-regular-400.06b3153d.woff"
+  },
+  {
+    "revision": "569693c9f5d42d769c7f87b90856219a",
+    "url": "/sportsstore/static/media/fa-regular-400.569693c9.woff2"
+  },
+  {
+    "revision": "e54c6ae126dd37d48c47dc1aa75cb54e",
+    "url": "/sportsstore/static/media/fa-regular-400.e54c6ae1.svg"
+  },
+  {
+    "revision": "323b749d60730fa9ab61ff2d70e1cc32",
+    "url": "/sportsstore/static/media/fa-regular-400.323b749d.ttf"
+  },
+  {
+    "revision": "686e245a4f1a9894d10a576655e932dd",
+    "url": "/sportsstore/static/media/fa-solid-900.686e245a.woff"
+  },
+  {
+    "revision": "59ea9019c9b9bc4d83ab9783e830735c",
+    "url": "/sportsstore/static/media/fa-solid-900.59ea9019.woff2"
+  },
+  {
+    "revision": "f9103ae53b2dbcb0a14605eebc90a2ce",
+    "url": "/sportsstore/static/media/fa-solid-900.f9103ae5.eot"
+  },
+  {
+    "revision": "b9c86e3abec102a2c9910dfac85c1c17",
+    "url": "/sportsstore/static/media/fa-solid-900.b9c86e3a.ttf"
+  },
+  {
+    "revision": "3e30e90a9ee4f94765599bcefd758cb3",
+    "url": "/sportsstore/static/media/fa-brands-400.3e30e90a.svg"
+  },
+  {
+    "revision": "1eba168c1b8603ab4dd282f5633d4650",
+    "url": "/sportsstore/static/media/fa-solid-900.1eba168c.svg"
+  },
+  {
+    "revision": "4e31bc998c1fbc445c1b7f7ccc139b70",
+    "url": "/sportsstore/index.html"
+  }
+];
